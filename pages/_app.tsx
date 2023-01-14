fd182dabde951e7a71da761ef0e183ca554626d3
Enkh-Amar-001 <96556227+Enkh-Amar-001@users.noreply.github.com>
2023-01-15 01:26:57 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,20 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  <Head>
-    <meta property="og:title" content="Streamhub Mongolia"/>
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://streamhub.mn" />
-    <meta property="og:image" content="https://streamhub.netlify.app/thumbnail.png" />
-    <meta property="og:image:width" content="1920" />
-    <meta property="og:image:height" content="1080" />
-    <meta property="og:description" content="A new way to make a donation easy and secure." />
-    <meta name="theme-color" content="#80f5d2" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="#80f5d2"></meta>
-  </Head>
-  return <Component {...pageProps} />
+  return (
+    <>  
+        <Head>
+        <meta property="og:title" content="Streamhub Mongolia"/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://streamhub.mn" />
+        <meta property="og:image" content="https://streamhub.netlify.app/thumbnail.png" />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:description" content="A new way to make a donation easy and secure." />
+        <meta name="theme-color" content="#80f5d2" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#80f5d2"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
